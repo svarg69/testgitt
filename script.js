b961f9247f93bb6e57e3c0d3345473a759060876
svarg69 <166533810+svarg69@users.noreply.github.com>
2024-07-09 21:05:16 +0300

    switch

--- script.js
+++ script.js
@@ -1,3 +1,122 @@
-const radius = +prompt('Введите радиус окружности', '');
-const square = 3.14 * radius ** 2;
-alert('Площать вашей окружности ' + square);
+// const radius = +prompt('Введите радиус окружности', '');
+// const square = 3.14 * radius ** 2;
+// alert('Площать вашей окружности ' + square);
+
+
+// операторы сранвения
+// x y
+
+// console.log (1 === '1');  строгое сравнение по типу данных
+
+// console.log(!false); возвращает обратное значение введенному
+
+// console.log(1 && 2);
+
+// console.log(1>= 0);
+
+// console.log(1 != "1");
+
+
+// if(2 < 0){
+//     alert('its true!');
+// } else {
+//     alert('its a lie!!!');
+// }
+
+// const x = +prompt('vvedite num');
+
+// if (x > 5){
+//     alert('bolshe 5');
+// } else if (x == 0){
+//     alert('ravno 0')
+// } else {
+//     alert('menshe 5 i ne ravno 0')
+// }
+
+// const x = +prompt('vvedite num');
+// if (x > 5 || x == 0){
+//     alert('on bolshe i ne raven 0');
+// } else {
+//     alert('libo menshe libo 0');
+// }
+
+// if (true && false){
+//     console.log(true);
+// }
+
+// if(true || false){
+//     console.log(true);
+// }
+
+// const x = +prompt('vvedite num');
+// let y = (x > 5) ? 10 : 0;
+// alert (y);
+// если икс больше пяти игрик 10, если нет то 0
+
+
+// const userAge = +prompt('vvedite age');
+
+// if (userAge > 0 && userAge <= 120){
+//     alert ('otlichno');
+// } else {
+//     alert ('u are false')
+// }
+
+
+// const x = +prompt('vvedite num');
+
+// if (x > 0) {
+//     alert(`мудаль числа ${x}`);
+// } else {
+//     alert(`модуль числа ${-x}`);
+// }
+
+
+// const hrs = +prompt('vvedite часы');
+// const min = +prompt('vvedite минуты');
+// const sec = +prompt('vvedite секунды');
+
+// if ((hrs >= 0 && hrs <= 23) && (min >= 0 && min <= 59) && (sec >= 0 && sec <= 59)){
+//     alert (`Московское время ${hrs} часов ${min} минут ${sec} секунд`);
+// } else {
+//     alert('Вы не в Москве');
+// }
+
+
+// const month = +prompt ('vvedi');
+
+// switch (month) {
+//     case 1:
+//         alert ('январь');
+//         break;
+//     case 2:
+//         alert ('февраль');
+//         break; 
+//     case 3:
+//         alert ('март');
+//         break;
+//     case 4:
+
+//         alert ('апрель');
+//             break;
+
+//     default:
+//         alert('Учи времена года!!')
+//         break;
+// }
+
+
+// const x = +prompt ('vvedi num');
+// const y = +prompt ('vvedi num');
+// const sign = prompt ('vvedi znak');
+
+// switch (sign) {
+//     case '+':
+//         alert (`${ x + y}`);
+//         break;
+//     case '-':
+//         alert (`${ x- y}`);
+//         break; 
+
+// }
+// например свич кейс применяется при входе юзера на сайт через свитч определяем кто он - админ, пользователь или еще ктото
